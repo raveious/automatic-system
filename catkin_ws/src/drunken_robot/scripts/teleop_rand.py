f636b28cb39f9264bcb5271fa22f5594e099c3e8
amkurup <amkurup@mtu.edu>
2017-01-22 15:35:10 -0500

    updating teleop with random movement

--- catkin_ws/src/drunken_robot/scripts/teleop_rand.py
+++ catkin_ws/src/drunken_robot/scripts/teleop_rand.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 
+# Talker that reads current location temperature and  
+# publishes std_msgs to the 'chatter' topic as Float32
+
+# Intro to Robotics - EE5900 - Spring 2017
+#          Assignment #2
+
+#       Project #2 Group #2
+#         Ian (Team Lead)
+#           Haden
+#           Akhil
+#
+# Revision: v1.1
+
+# imports
 import rospy
+import random
 from geometry_msgs.msg import Twist
 
 last_twist_time_time = 0.0
@@ -14,6 +29,16 @@ def twistCallback(data):
     rospy.loginfo("Received twist message")
 
 
+def randMovement():
+    # generate random movement mapping
+    map = [random.randrange(-1,2), random.randrange(-1,2)]
+    # push Twist msgs
+    Twist.linear.x = map[0]
+    Twist.angular.z = map[1]
+    # publish Twist
+    pub.publish(Twist)
+
+
 def setup():
      # create node for listening to twist messages
     rospy.init_node("teleop_rand", anonymous=True)
@@ -22,13 +47,14 @@ def setup():
     rospy.Subscriber("cmd_vel", Twist, twistCallback)
     rate = rospy.Rate(5)
 
-    pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+    pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
 
     while not rospy.is_shutdown():
         if last_twist_time_time + delay < rospy.get_time():
             rospy.loginfo("drunk mode")
         else:
             rospy.loginfo("waiting for input to stop")
+
         rate.sleep()
 
 
