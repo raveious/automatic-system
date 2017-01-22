a2d46c4594c5507657fbedee673d8e56a5cdc4d5
amkurup <amkurup@mtu.edu>
2017-01-22 17:10:26 -0500

    adding parameters

--- catkin_ws/src/drunken_robot/scripts/teleop_rand.py
+++ catkin_ws/src/drunken_robot/scripts/teleop_rand.py
@@ -16,33 +16,66 @@
 # imports
 import rospy
 import random
+import sys
 from geometry_msgs.msg import Twist
 
 last_twist_time_time = 0.0
 delay = 2.0
 
+# Read command line parameters and form key map
+# Usage: teleop_rand.py <rate> <linear_min> <linear_max> <angular_min> <angular_max>
+def readParameters():
+    global user_rate, linear_min, linear_max, angular_min, angular_max
+
+    # default assignments
+    user_rate = 2
+    linear_min = -1
+    linear_max = 2
+    angular_min = -1
+    angular_max = 2
+
+    # check args
+    if len(sys.argv)>1:
+        user_rate = int(sys.argv[1])
+
+    if len(sys.argv)>2:
+        linear_min = int(sys.argv[2])
+
+    if len(sys.argv)>3:
+        linear_max = int(sys.argv[3])
+
+    if len(sys.argv)>4:
+        angular_min = int(sys.argv[4])
+   
+    if len(sys.argv)>5:
+        angular_max = int(sys.argv[5])
+
+
 
 def twistCallback(data):
     global last_twist_time_time
-
     last_twist_time_time = rospy.get_time()
 
 
 def setup():
+    readParameters()
+
      # create node for listening to twist messages
     rospy.init_node("teleop_rand")
 
     # subscribe to all
     rospy.Subscriber("cmd_vel", Twist, twistCallback)
-    rate = rospy.Rate(5)
+    rate = rospy.Rate(user_rate)
 
     pub = rospy.Publisher("/jackal_velocity_controller/cmd_vel", Twist, queue_size=10)
 
     while not rospy.is_shutdown():
+
         # check for keyboard teleop
         if last_twist_time_time + delay < rospy.get_time():
             # generate random movement mapping
-            map = [random.randrange(-1,2), random.randrange(-1,2)]
+            map = [random.randrange(linear_min,linear_max), random.randrange(angular_min,angular_max)]
+            # map = [random.randrange(-1,2), random.randrange(-1,2)]
 
             motion = Twist()
             # push Twist msgs
@@ -52,8 +85,10 @@ def setup():
             # publish Twist
             pub.publish(motion)
             pub = rospy.Publisher("/jackal_velocity_controller/cmd_vel", Twist, queue_size=10)
-
-            rospy.logdebug("drunk mode x = {} y = {}".format(motion.linear.x, motion.linear.y))
+            
+            # rospy.loginfo('user_rate=%d  linear_min=%d  linear_max=%d  angular_min=%d  angular_max=%d'%(user_rate, linear_min, linear_max, angular_min, angular_max))
+            # rospy.loginfo("drunk mode x = {} z = {}".format(motion.linear.x, motion.angular.z))
+            rospy.logdebug("drunk mode x = {} z = {}".format(motion.linear.x, motion.angular.z))
         else:
             rospy.logdebug("waiting for input to stop")
 
