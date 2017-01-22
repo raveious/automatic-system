f4f5770b817c555f4b9d9a9547b2c4bab45a7d23
spartanhaden <spartanhaden@gmail.com>
2017-01-22 07:26:48 -0500

    Added timeout and waiting for teleop commands to stop publishing. Sleep time.

--- catkin_ws/src/drunken_robot/scripts/teleop_rand.py
+++ catkin_ws/src/drunken_robot/scripts/teleop_rand.py
@@ -3,9 +3,15 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+last_twist_time_time = 0.0
+delay = 2.0
+
 
 def twistCallback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.linear.x)
+    global last_twist_time_time
+
+    last_twist_time_time = rospy.get_time()
+    rospy.loginfo("Received twist message")
 
 
 def setup():
@@ -14,12 +20,17 @@ def setup():
 
     # subscribe to all
     rospy.Subscriber("cmd_vel", Twist, twistCallback)
-    rate = rospy.Rate(100)
+    rate = rospy.Rate(5)
 
     pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
-    # spin() keeps python from exiting until this node is stopped
-    rospy.spin()
+    while not rospy.is_shutdown():
+        if last_twist_time_time + delay < rospy.get_time():
+            rospy.loginfo("drunk mode")
+        else:
+            rospy.loginfo("waiting for input to stop")
+        rate.sleep()
+
 
 
 if __name__ == "__main__":
